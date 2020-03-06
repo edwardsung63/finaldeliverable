22dabbac27c71f3358d3373829b354157f04d612
Danell F <danellf@uw.edu>
2020-03-06 10:30:48 -0800

    Messing with the UI

--- app_ui.R
+++ app_ui.R
@@ -1,8 +1,14 @@
-ui <- fluidPage(
-  h1("Hello App"),
-  textInput(
-    inputId = "username",
-    label = h3("Type in your name")
-  ),
-  textOutput(outputId = "out_message")
+ui <- navbarPage(tabPanel("Main tab", h1("Main page")),
+                 tabPanel("tab 1", h1("test tab 1")),
+                 tabPanel("tab 2", h1("test tab 2"))
 )
+
+# ui <- fluidPage(
+#   h1("Hello App"),
+#   textInput(
+#     inputId = "username",
+#     label = h3("Type in your name")
+#   ),
+#   textOutput(outputId = "out_message")
+# )
+
