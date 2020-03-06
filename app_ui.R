3142f108c639626dae5c6112fecb80a553f92ddc
Danell F <danellf@uw.edu>
2020-03-06 10:39:42 -0800

    Basic layout

--- app_ui.R
+++ app_ui.R
@@ -1,14 +1,21 @@
-ui <- navbarPage(tabPanel("Main tab", h1("Main page")),
-                 tabPanel("tab 1", h1("test tab 1")),
-                 tabPanel("tab 2", h1("test tab 2"))
+tab_1 <- sidebarLayout(
+  sidebarPanel(h1("This is a main sidebar")),
+  mainPanel(h1("This is the main panel"))
 )
 
-# ui <- fluidPage(
-#   h1("Hello App"),
-#   textInput(
-#     inputId = "username",
-#     label = h3("Type in your name")
-#   ),
-#   textOutput(outputId = "out_message")
-# )
+main <- fluidPage(
+  h1("Hello App"),
+  textInput(
+    inputId = "username",
+    label = h3("Type in your name")
+  ),
+  textOutput(outputId = "out_message")
+)
+
+ui <- navbarPage("INFO 201 Final",
+  tabPanel("Main table", main),
+  tabPanel("tab 1", tab_1),
+  tabPanel("tab 2", h1("test tab 2"))
+)
+
 
